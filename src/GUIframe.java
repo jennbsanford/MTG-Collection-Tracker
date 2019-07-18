85c8037374102f4f8a65787e67e63cb0204e4ae9
Dylan <dtlvoigt@gmail.com>
2019-07-18 16:32:27 -0700

    added test line to feed decklist to deck field class

--- src/GUIframe.java
+++ src/GUIframe.java
@@ -1,19 +1,38 @@
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 //public class GUIframe
 public class GUIframe extends JFrame
 {
     protected GUIpanel mainPanel;
+    protected GUIcontainer contentPane;/////
 
-    public GUIframe()
+    public GUIframe() throws Exception
     {
         //set title and have program exit when window is closed
         super("MTG Collection Tracker");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         //create panel and add to frame
         mainPanel = new GUIpanel();
         add(mainPanel);
+
+
+        //contentPane = new GUIcontainer();
+        //add(contentPane);
+
+        //////////////
+        mainPanel.deck.currentDeck = "C:\\Users\\Dylan\\Desktop\\Decks\\deck1.txt";
+        try {
+            mainPanel.deck.loadDeck();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + "hello");
+        }//////////////
 
         //show frame on screen
         pack();
