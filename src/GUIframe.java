f2bb80b58d0158d004fd9dd2fe547e33809338e0
Dylan <dtlvoigt@gmail.com>
2019-07-25 10:33:06 -0700

    reorganized constructor method

--- src/GUIframe.java
+++ src/GUIframe.java
@@ -3,11 +3,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-//public class GUIframe
 public class GUIframe extends JFrame
 {
     protected GUIpanel mainPanel;
-    protected GUIcontainer contentPane;/////
+    //protected GUIcontainer contentPane;
 
     public GUIframe() throws Exception
     {
@@ -15,24 +14,12 @@ public class GUIframe extends JFrame
         super("MTG Collection Tracker");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
         //create panel and add to frame
         mainPanel = new GUIpanel();
         add(mainPanel);
 
-
         //contentPane = new GUIcontainer();
         //add(contentPane);
-
-        //////////////
-        //mainPanel.deck.currentDeck = "C:\\Users\\Dylan\\Desktop\\Decks\\deck1.txt";
-        try {
-            //mainPanel.deck.loadDeck();
-        }
-        catch(Exception e)
-        {
-            System.out.println(e + "frame");
-        }//////////////
 
         //show frame on screen
         pack();
@@ -42,8 +29,5 @@ public class GUIframe extends JFrame
     public static void main(String[] args)
     {
         System.out.print("Entered frame class main.");//////////
-        //GUIpanel mainPanel = new GUIpanel();
-        //initFrame();
-
     }
 }
