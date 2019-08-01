6b4a4e21bed13ccf7fd32d99cfdfc4fdbddd3b2e
Dylan <dtlvoigt@gmail.com>
2019-08-01 15:44:53 -0700

    Frame now has action listening functionality

--- src/GUIframe.java
+++ src/GUIframe.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUIframe extends JFrame
+public class GUIframe extends JFrame implements ActionListener
 {
     protected GUIpanel mainPanel;
 
@@ -13,6 +15,7 @@ public class GUIframe extends JFrame
         //create panel and add to frame
         mainPanel = new GUIpanel();
         add(mainPanel);
+        //mainPanel.createDeck.addActionListener(this);
 
         //show frame on screen
         pack();
@@ -23,5 +26,10 @@ public class GUIframe extends JFrame
     public static void main(String[] args)
     {
         System.out.print("Entered frame class main.");//////////
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        System.out.println("YOOOO");
     }
 }
