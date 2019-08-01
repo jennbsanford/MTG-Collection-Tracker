2eec4d04083c155ece5ea6e6df561265e197ff29
Dylan <dtlvoigt@gmail.com>
2019-08-01 11:15:43 -0700

    created 'new deck' button

---
+++ src/GUIcreateDeckButton.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIcreateDeckButton extends JButton
+{
+    GUIcreateDeckButton(GridBagConstraints c)
+    {
+        super("+");
+        setToolTipText("Create a new deck");
+        //c.gridwidth = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.BOTH;
+        //c.gridheight = GridBagConstraints.NORTH;
+        c.gridx = 3;
+        c.gridy = 1;
+        //c.fill = GridBagConstraints.NONE;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.insets = new Insets(5, 1, 5,1);
+    }
+}
