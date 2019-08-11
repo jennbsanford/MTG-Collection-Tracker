a95beeaedd1708ae9d86a59e402c1d2be98dba41
Dylan <dtlvoigt@gmail.com>
2019-08-11 11:37:41 -0700

    created add to sideboard button

---
+++ src/GUIaddToDeckSideButton.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIaddToDeckSideButton extends JButton
+{
+    GUIaddToDeckSideButton(GridBagConstraints c)
+    {
+        super("+ Side");
+        setToolTipText("Add entered card to the sideboard");
+        c.fill = GridBagConstraints.BOTH;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridx = 19;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.insets = new Insets(5, 2, 5,2);
+    }
+}
